b63ee3305404c7fb29c0be97df4aed0cc8e78d4a
Lier0 <28351075+Lier0@users.noreply.github.com>
2020-11-16 16:50:07 +0100

    Update Discord.php
    Fix https://cdn.discordapp.com/avatars/0.png, ie discord user without avatar trying to sign in.

--- src/Providers/Discord.php
+++ src/Providers/Discord.php
@@ -55,11 +55,12 @@ class Discord extends Provider
         $this->verifyEmail($email = $user->getEmail());
 
         $hash = $user->getAvatarHash();
-        $file = $hash ? "{$user->getId()}/{$user->getAvatarHash()}.png" : '0.png';
+        $file = $hash ? "{$user->getId()}/{$user->getAvatarHash()}.png" : null;
+        $avatar = ($file) ? "https://cdn.discordapp.com/avatars/$file" : "https://cdn.discordapp.com/embed/avatars/0.png";
 
         $registration
             ->provideTrustedEmail($email)
-            ->provideAvatar("https://cdn.discordapp.com/avatars/$file")
+            ->provideAvatar($avatar)
             ->suggestUsername($user->getUsername() ?: '')
             ->setPayload($user->toArray());
     }
