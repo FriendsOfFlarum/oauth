adf8e7fc0716a6889103e85794b35b5ba24f5b09
Lier0 <28351075+Lier0@users.noreply.github.com>
2020-11-18 00:55:30 +0100

    Update Discord.php
    Remove avatar var.

--- src/Providers/Discord.php
+++ src/Providers/Discord.php
@@ -55,12 +55,13 @@ class Discord extends Provider
         $this->verifyEmail($email = $user->getEmail());
 
         $hash = $user->getAvatarHash();
-        $file = $hash ? "{$user->getId()}/{$user->getAvatarHash()}.png" : null;
-        $avatar = $file ? "https://cdn.discordapp.com/avatars/$file" : "https://cdn.discordapp.com/embed/avatars/0.png";
+        $file = $hash ?
+            "https://cdn.discordapp.com/avatars/{$user->getId()}/{$user->getAvatarHash()}.png"
+            : "https://cdn.discordapp.com/embed/avatars/0.png";
 
         $registration
             ->provideTrustedEmail($email)
-            ->provideAvatar($avatar)
+            ->provideAvatar($file)
             ->suggestUsername($user->getUsername() ?: '')
             ->setPayload($user->toArray());
     }
