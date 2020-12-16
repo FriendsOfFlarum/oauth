6a8230846b7a98afc3e1c9dc24422afe4a2b17ce
Ian Morland <ian@morland.me>
2020-12-16 12:57:13 +0000

    lint

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,5 +1,5 @@
 import AuthSettingsPage from './components/AuthSettingsPage';
 
 app.initializers.add('fof/oauth', () => {
-  app.extensionData.for('fof-oauth').registerPage(AuthSettingsPage);
+    app.extensionData.for('fof-oauth').registerPage(AuthSettingsPage);
 });
