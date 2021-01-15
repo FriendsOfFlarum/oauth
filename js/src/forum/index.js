6f4d64f6f52959be3da16e4fdca1101a000a4e0d
Clark Winkelmann <clark.winkelmann@gmail.com>
2021-01-15 19:09:55 +0100

    Remove cached ItemList which is against Mithril standards This fixes LogInButton class being added one more time each time the modal is open

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,34 +1,25 @@
 import { extend, override } from 'flarum/extend';
 import LogInButtons from 'flarum/components/LogInButtons';
 import LogInButton from 'flarum/components/LogInButton';
-import ItemList from 'flarum/utils/ItemList';
 import extractText from 'flarum/utils/extractText';
 
 app.initializers.add('fof/oauth', () => {
     const onlyIcons = !!Number(app.data['fof-oauth.only_icons']);
 
-    let buttons = null;
-
     extend(LogInButtons.prototype, 'items', function (items) {
-        if (!buttons) {
-            buttons = new ItemList();
-
-            app.forum
-                .attribute('fof-oauth')
-                .filter(Boolean)
-                .forEach(({ name, icon }) =>
-                    buttons.add(
-                        name,
-                        <LogInButton className={`Button LogInButton--${name}`} icon={icon} path={`/auth/${name}`}>
-                            {app.translator.trans(`fof-oauth.forum.log_in.with_button`, {
-                                provider: app.translator.trans(`fof-oauth.lib.providers.${name}`),
-                            })}
-                        </LogInButton>
-                    )
-                );
-        }
-
-        items.merge(buttons);
+        app.forum
+            .attribute('fof-oauth')
+            .filter(Boolean)
+            .forEach(({name, icon}) =>
+                items.add(
+                    name,
+                    <LogInButton className={`Button LogInButton--${name}`} icon={icon} path={`/auth/${name}`}>
+                        {app.translator.trans(`fof-oauth.forum.log_in.with_button`, {
+                            provider: app.translator.trans(`fof-oauth.lib.providers.${name}`),
+                        })}
+                    </LogInButton>
+                )
+            );
     });
 
     if (onlyIcons) {
