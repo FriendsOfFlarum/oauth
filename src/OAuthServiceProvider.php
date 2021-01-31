022ce9e51649725b8ffddc63a36672e3beefe0fa
luuhai48 <luuhai.hn48@gmail.com>
2021-01-31 21:43:36 +0700

    Add support for LinkedIn

--- src/OAuthServiceProvider.php
+++ src/OAuthServiceProvider.php
@@ -24,6 +24,7 @@ class OAuthServiceProvider extends ServiceProvider
             Providers\GitLab::class,
             Providers\Twitter::class,
             Providers\Google::class,
+            Providers\LinkedIn::class,
         ], 'fof-oauth.providers');
 
         $this->app->singleton('fof-oauth.providers.forum', $this->map(function (Provider $provider) {
---
+++ src/Providers/LinkedIn.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of fof/oauth.
+ *
+ * Copyright (c) 2020 FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace FoF\OAuth\Providers;
+
+use Flarum\Forum\Auth\Registration;
+use Flarum\Settings\SettingsRepositoryInterface;
+use FoF\OAuth\Provider;
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\LinkedIn as LinkedInProvider;
+
+
+class LinkedIn extends Provider
+{
+    /**
+     * @var SettingsRepositoryInterface
+     */
+    protected $settings;
+
+    public function __construct(SettingsRepositoryInterface $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    /**
+     * @var LinkedInProvider
+     */
+    protected $provider;
+
+    public function name(): string
+    {
+        return 'linkedin';
+    }
+
+    public function link(): string
+    {
+        return 'https://linkedin.com/developers/apps/new';
+    }
+
+    public function fields(): array
+    {
+        return [
+            'client_id'     => 'required',
+            'client_secret' => 'required',
+        ];
+    }
+
+    public function provider(string $redirectUri): AbstractProvider
+    {
+        return $this->provider = new LinkedInProvider([
+            'clientId'        => $this->getSetting('client_id'),
+            'clientSecret'    => $this->getSetting('client_secret'),
+            'redirectUri'     => $redirectUri,
+        ]);
+    }
+
+    public function suggestions(Registration $registration, $user, string $token)
+    {
+        $this->verifyEmail($email = $user->getEmail());
+
+        $registration
+        ->provideTrustedEmail($email)
+        ->suggestUsername($user->getFirstName())
+        ->setPayload($user->toArray());
+
+        $avatar = $user->getImageUrl();
+        if ($avatar) {
+            $registration->provideAvatar($avatar);
+        }
+    }
+}
