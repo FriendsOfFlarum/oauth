af621b05c89092a6b953071306eb556c12a2c862
Ian Morland <ian@morland.me>
2022-05-03 22:22:22 +0100

    Move provider settings to ItemList, export settings page

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -5,3 +5,5 @@ import AuthSettingsPage from './components/AuthSettingsPage';
 app.initializers.add('fof/oauth', () => {
   app.extensionData.for('fof-oauth').registerPage(AuthSettingsPage);
 });
+
+export { AuthSettingsPage };
