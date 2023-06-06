9c18eeafbf1363b95e551b1a49fea7fdd9ad2407
IanM <16573496+imorland@users.noreply.github.com>
2023-06-06 07:55:18 +0100

    chore: make config button shareable (#57)

--- js/src/admin/components/ConfigureWithOAuthPage.js
+++ js/src/admin/components/ConfigureWithOAuthPage.js
@@ -1,6 +1,5 @@
-import app from 'flarum/admin/app';
 import ExtensionPage from 'flarum/admin/components/ExtensionPage';
-import LinkButton from 'flarum/common/components/LinkButton';
+import ConfigureWithOAuthButton from './ConfigureWithOAuthButton';
 
 /**
  * The `ConfigureWithOAuthPage` component is meant for 3rd party extensions to provide a handy link to `fof/oauth` settings.
@@ -16,9 +15,7 @@ export default class ConfigureWithOAuthPage extends ExtensionPage {
       <div className="container">
         <div className="OAuthSettingsPage">
           <br />
-          <LinkButton className="Button" href={app.route('extension', { id: 'fof-oauth' })}>
-            {app.translator.trans('fof-oauth.admin.configure_button_label')}
-          </LinkButton>
+          <ConfigureWithOAuthButton />
         </div>
       </div>,
     ];
--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -2,9 +2,10 @@ import app from 'flarum/admin/app';
 
 import AuthSettingsPage from './components/AuthSettingsPage';
 import ConfigureWithOAuthPage from './components/ConfigureWithOAuthPage';
+import ConfigureWithOAuthButton from './components/ConfigureWithOAuthButton';
 
 app.initializers.add('fof/oauth', () => {
   app.extensionData.for('fof-oauth').registerPage(AuthSettingsPage);
 });
 
-export { AuthSettingsPage, ConfigureWithOAuthPage };
+export { AuthSettingsPage, ConfigureWithOAuthPage, ConfigureWithOAuthButton };
