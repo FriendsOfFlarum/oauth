cdb1610ded437979f5f4ecb29c798fd42a441edd
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2024-10-30 18:02:08 +0100

    chore: unused

--- js/src/forum/components/LinkedAccounts.tsx
+++ js/src/forum/components/LinkedAccounts.tsx
@@ -31,7 +31,7 @@ export default class LinkedAccounts extends Component<IAttrs, IState> {
     this.loadLinkedAccounts();
   }
 
-  view(vnode: Mithril.Vnode<IAttrs, this>) {
+  view() {
     const linkedAccounts = app.store.all<LinkedAccount>('linked-accounts');
 
     return (
