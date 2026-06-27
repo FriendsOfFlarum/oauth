821e2237c8571e8be1e8ee284dcb7cd6c5496db3
ElfTorg <elftorg@gmail.com>
2026-06-27 20:14:16 +0300

    Apply provider icons in runtime login patch

--- js/dist/forum-login-order.js
+++ js/dist/forum-login-order.js
@@ -3,6 +3,21 @@
     return module && module.__esModule ? module.default : module;
   }
 
+  var providerIcons = {
+    discord: 'fab fa-discord',
+    facebook: 'fab fa-facebook-f',
+    github: 'fab fa-github',
+    gitlab: 'fab fa-gitlab',
+    google: 'fab fa-google',
+    linkedin: 'fab fa-linkedin-in',
+    twitter: 'fab fa-twitter',
+  };
+
+  function providerNameFromClass(className) {
+    var match = className && className.match(/LogInButton--([A-Za-z0-9_-]+)/);
+    return match ? match[1] : null;
+  }
+
   var app = defaultExport(flarum.core.compat['forum/app']);
   var extension = flarum.core.compat['common/extend'];
   var LogInButtons = defaultExport(flarum.core.compat['forum/components/LogInButtons']);
@@ -12,8 +27,16 @@
 
   app.initializers.add('fof/oauth-login-order', function () {
     extension.extend(LogInButton, 'initAttrs', function (_, attrs) {
-      if (attrs.className && attrs.className.indexOf('FoFLogInButton') !== -1 && attrs.className.indexOf('Button--block') === -1) {
-        attrs.className = attrs.className.replace('Button ', 'Button Button--block ');
+      if (attrs.className && attrs.className.indexOf('FoFLogInButton') !== -1) {
+        var providerName = providerNameFromClass(attrs.className);
+
+        if (providerName && providerIcons[providerName]) {
+          attrs.icon = providerIcons[providerName];
+        }
+
+        if (attrs.className.indexOf('Button--block') === -1) {
+          attrs.className = attrs.className.replace('Button ', 'Button Button--block ');
+        }
       }
     });
 
