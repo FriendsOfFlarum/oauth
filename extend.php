6e60b07a4de5c7b6d5a606faac11e8f6b1f218eb
ElfTorg <elftorg@gmail.com>
2026-06-27 19:37:58 +0300

    Register OAuth login order runtime patch

--- extend.php
+++ extend.php
@@ -24,6 +24,7 @@ use FoF\Extend\Events\OAuthLoginSuccessful;
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
+        ->js(__DIR__.'/js/dist/forum-login-order.js')
         ->css(__DIR__.'/resources/less/forum.less'),
 
     (new Extend\Frontend('admin'))
